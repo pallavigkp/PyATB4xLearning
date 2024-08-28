218c9a647af7fe452e4f6d684602a02acda02c27
pallavigkp <pallavisingh111220@gmail.com>
2024-08-28 17:00:29 +0530

    Lab005.py added

---
+++ src/ex_10082024/Lab005.py
@@ -0,0 +1,8 @@
+#syntax error in programming
+#source code = human readable code or codewritten by humans
+#we have to follow some rules while writing code.
+#1+1=2
+#it will show syntax error here we have to give command
+print("1+1=2")
+print("hello python world" ,sep="?")
+print("punit", "prince", "noida", sep="/")
