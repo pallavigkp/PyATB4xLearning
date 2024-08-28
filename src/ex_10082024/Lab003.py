af90990a9213fe2867be9bcbde9267a57c4f0a41
pallavigkp <pallavisingh111220@gmail.com>
2024-08-28 13:03:12 +0530

    Lab003.py file added

---
+++ src/ex_10082024/Lab003.py
@@ -0,0 +1,9 @@
+print("hello world")
+# there are some in built commands in python Builtin.py all the important already built by python.
+#print()
+# self a concept in oops which points to itself
+# *arg - unlimited numbers of argument  *- string ,int,float,boolean
+#file
+#sep -' '= how you want to separate the arguments
+#end ='\n' = in the end what you want to write or add or to do
+print("hello","WORLD","gorakhpur",123, 3.14, True, sep ="-")
