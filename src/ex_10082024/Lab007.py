b3d7e89d8359d23025ad1cf86c1203cc291a1c9c
pallavigkp <pallavisingh111220@gmail.com>
2024-08-28 17:43:50 +0530

    Lab007.py added

---
+++ src/ex_10082024/Lab007.py
@@ -0,0 +1,7 @@
+#if we have to find the maximum or minimum value of between two integers
+# then the following command will be used
+print(max(2,3,76,34,100,603,42,64,-1,-100,-300))
+print(min(2,3,76,34,100,603,42,64,-1,-100,-300))
+#but if we put integers and string value together it will give error
+print(max(2,3,76,34,100,603,42,64,-1,-100,-300))
+print(min(2,3,76,34,100,603,42,64,-1,-100,-300, "pramod", "*"))
---
+++ src/ex_10082024/Lab008.py
@@ -0,0 +1,6 @@
+#python keywords
+#keywords are generally reserved words  which can not be used in variables names function name, or any other identifier
+#it can be case sensitive
+#upper case or lower case
+import keyword
+print (keyword.kwlist)
