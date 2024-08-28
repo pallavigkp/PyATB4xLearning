9551e824f643223b6b473697c0ce093041be8658
pallavigkp <pallavisingh111220@gmail.com>
2024-08-28 17:22:03 +0530

    Lab006.py added

---
+++ src/ex_10082024/Lab006.py
@@ -0,0 +1,3 @@
+#if we have to find the maximum or minimum value of between two integers then the following command will be used
+print(max(2,3,76,34,100,603,42,64,-1,-100,-300))
+print(min(2,3,76,34,100,603,42,64,-1,-100,-300))
