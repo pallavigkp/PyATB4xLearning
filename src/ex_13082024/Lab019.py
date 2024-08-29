e15894fe7e88e40f8d0b9165fd08ae1d96510262
pallavigkp <pallavisingh111220@gmail.com>
2024-08-29 18:43:16 +0530

    Lab018.py added

---
+++ src/ex_13082024/Lab019.py
@@ -0,0 +1,17 @@
+"""here identifiers act as a variable name
+where as literals act as variables value
+so if a=123,then a will be identifiers and 123 will be literals"""
+age=65
+"""identifiers Rules
+They start with a letter (a-z) or (A-Z) 
+an UNDERSCORE(_) FOLLOWED BY zero or more letters
+python is a case sensitive language"""
+a=10
+A=20
+#123=20
+#AS PER RULES VARIABLE NAME CAN NOT initiate WITH NUMBERS OR ANY OTHER CHARACTERS
+print(a)
+print(A)
+print(age)
+pi=3.14
+print(pi+1)
