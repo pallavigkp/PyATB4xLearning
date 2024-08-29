1333a7de2b1e0f32aaf8f5ee2eacfd256cde84fc
pallavigkp <pallavisingh111220@gmail.com>
2024-08-29 08:05:21 +0530

    Lab010.py added

---
+++ src/ex_10082024/Lab010.py
@@ -0,0 +1,10 @@
+#dynimic type
+#at the run time data of variable can be changed
+#you don't need to mention the datatype
+#python will automatically take the data type
+a=10
+age=45
+print(type(age))
+age="pramod"
+print(age)
+print(type(age))
