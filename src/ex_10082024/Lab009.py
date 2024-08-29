7a6dc8f4de2e2c0c8d1e681fe09d24913ac10c8b
pallavigkp <pallavisingh111220@gmail.com>
2024-08-29 07:41:32 +0530

    Lab009.py added

---
+++ src/ex_10082024/Lab009.py
@@ -0,0 +1,12 @@
+#they start with a letter between a-z or A-Z
+#or an (_)underscore followed by zero or more letters,underscore and digits (0-9)
+#case sensitive language so MY and my both are two different variables
+a =24
+b= 0
+_= 1
+print(a,b,_)
+_=_+1
+print(_)
+abc123= 23
+#123abc=23 will give error as per the rule variable start with a-z or A-Z OR (_) UNDERSCORE
+print(abc123)
