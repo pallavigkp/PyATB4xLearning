4556cfdec8aa983bdedefd33815ed8e30c7a01a5
pallavigkp <pallavisingh111220@gmail.com>
2024-08-29 13:24:35 +0530

    Lab014.py added

---
+++ src/ex_13082024/Lab014.py
@@ -0,0 +1,12 @@
+#revision how to find maximum and minimum value in between
+print(max(1,4,6,20,-40,50,103,1000,50013,57,64364,66489))
+print(min(1,4,6,20,-40,50,103,1000,50013,57,64364,66489))
+
+#boolean data type
+a= "true"
+b="false"
+print(a)
+print(b)
+#data type finding code
+print(type(a))
+print(type(b))
