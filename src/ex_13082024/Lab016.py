41eef61fb15a08e535c1506d0191f6525ad71650
pallavigkp <pallavisingh111220@gmail.com>
2024-08-29 13:36:23 +0530

    Lab016.py added

---
+++ src/ex_13082024/Lab016.py
@@ -0,0 +1,16 @@
+#data type
+from operator import truediv
+#integer type of data
+age=36
+#string data type
+candidate="Punit"
+#float data type
+A= 3.14
+#boolean data type
+a="true"
+b="false"
+print(a,age,candidate,A,b)
+print(type(a))
+print(type(candidate))
+print(type(A))
+print(type(age))
