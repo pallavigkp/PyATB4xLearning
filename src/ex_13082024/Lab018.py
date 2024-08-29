4a5c9fc23239bd9affddf1461e611440c185c7c7
pallavigkp <pallavisingh111220@gmail.com>
2024-08-29 18:24:35 +0530

    Lab018.py added

---
+++ src/ex_13082024/Lab018.py
@@ -0,0 +1,15 @@
+#there are several functions in python
+# generally we use in maths
+#max()
+#min()
+#pow()
+
+# power is just like giving powers to the numbers
+#here 2 to power of 3
+a=pow(2,3)
+print(a)
+#abs()
+#it will give absolute value of any number
+b=abs(-1)
+print(b)
+
