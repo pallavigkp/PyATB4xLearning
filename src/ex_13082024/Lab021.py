cbcc0708af8e5808f339c764fc995352c0934200
pallavigkp <pallavisingh111220@gmail.com>
2024-08-30 13:39:08 +0530

    Lab021.py added

---
+++ src/ex_13082024/Lab021.py
@@ -0,0 +1,11 @@
+#calculator program by taking input from user
+numb1=input("enter numb1")
+numb2=input("enter numb2")
+print(type(numb1+numb2))
+#it will join the number and return 34
+#because here input is string data type
+#we can not add,multiply,substract or divide string data type
+#so we have to convert them into integer data type
+num1= int(input("enter num1"))
+num2= int(input("enter num2"))
+print(type(num1))
