25699eb5b83c640fdce211e358d68d5882c441fe
pallavigkp <pallavisingh111220@gmail.com>
2024-08-31 14:04:41 +0530

    Lab022.py added

---
+++ src/ex_13082024/Lab022.py
@@ -0,0 +1,9 @@
+#take a user input a,b and calculate sum, div,sub,mul
+numb1=int(input("enter your number1"))
+numb2=int(input("enter your number2"))
+print("sum is",numb1+numb2)
+print("sub is",numb1-numb2)
+print("mul",numb1*numb2)
+print("div is",numb1/numb2)
+print(type(numb2))
+print(type(numb1))
