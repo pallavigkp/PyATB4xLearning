b620918e6a0cfdeb3fea2033d6fda2a6564b683d
pallavigkp <pallavisingh111220@gmail.com>
2024-09-03 13:47:33 +0530

    Lab032.py added

---
+++ src/ex_17082024/Lab031.py
@@ -0,0 +1,31 @@
+"""'//'--> (x//y) it will divide the first operands by the 2nd.
+also called as floor division"""
+print(10//3)
+#3|10|3 here 3 is quotient
+ # | 9|
+ #   1 ---> remainder
+print(-5//2)
+#2|5|2
+ # |4|
+ #   1
+print(5.0//2)
+print(-5.0//2)
+#so when we run this program it will return quotient value not the remainder value
+
+# '%' -->(x%y) it will return the remainder when the first operand is divided by the 2nd.
+print(10%3)
+print(20%6)
+print(24%5)
+#when we run this program it will return the value of remainder not the quotient
+# '**'--> (x**y)returns first raised to power second"""
+#**---> power
+print(2**2)
+print(2**3)
+print(2**4)
+print(2**4)
+print(2**5)
+print(2**6)
+
+#compare operator
+print(2==2)
+print(2==3)
