c65b4cb52f8791acd61803daf45ba03750628ac3
pallavigkp <pallavisingh111220@gmail.com>
2024-09-08 07:52:45 +0530

    Lab032.py added

---
+++ src/ex_17082024/Lab032.py
@@ -0,0 +1,21 @@
+"""not operators always return opposite or reverse result
+if value is true then output will be false"""
+
+is_pallavi_married= "true"
+print(not is_pallavi_married)
+print(is_pallavi_married)
+
+#logical or comparison operator
+"""always return boolean data type true/false
+Basically follows AND, OR gate
+AND----> if both the variable return true
+OR----> if either one variable is true"""
+#they basically follow AND ,OR gate
+a=(14>=7)
+b=(7>=14)
+c=(14>=7)
+d=(14==7)
+print(a)
+print(b)
+print(c)
+print(d)
