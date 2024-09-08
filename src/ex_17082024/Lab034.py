96b36ba98d8ec46bc6b9039d5d0a2e59ecc7688c
pallavigkp <pallavisingh111220@gmail.com>
2024-09-08 20:26:03 +0530

    Lab034.py added

---
+++ src/ex_17082024/Lab034.py
@@ -0,0 +1,6 @@
+#ternary operator
+"""if/else conditions"""
+my_age=int(input("Enter your Age"))
+print("I will go goa"if my_age >18 else"Cant go ,stay home")
+Age=int(input("Enter your age"))
+print("i can appear in entrance exam"if Age>18 else"not eligible for exam.")
---
+++ src/ex_17082024/Lab035.py
@@ -0,0 +1,18 @@
+#write a program to calculate
+#area of circle given by its radius using the formula
+#area=pi*r2 (take pi as 3.14
+#logic building formula
+#step1 Figure out the inputs and outputs
+#input--> r-->data type-->float or user will give
+#pi-->3.14
+#power-->pow or **-->any
+#o/p-->float-area,print area
+#step2
+#rough logic=area=3.14*pow(r,2)
+#step3-write the logic
+radius=float(input("Enter the radius of the circle\n"))
+print(radius)
+print(math.pi)
+area= math.pi * math.pow(radius,_y:2)
+print("area of the circle is-->"area)
+print("area of the circle is -->{area:.2f")
